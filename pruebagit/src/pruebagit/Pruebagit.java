4667265c25d808c9737f91246d9e33582a6f8303
Daniel Vela <veladan@gmail.com>
2014-03-20 13:51:15 +0100

    Trabajo completado

--- pruebagit/src/pruebagit/Pruebagit.java
+++ pruebagit/src/pruebagit/Pruebagit.java
@@ -16,7 +16,8 @@ public class Pruebagit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       PruebaDVA objeto = new PruebaDVA();
+       objeto.imprimeNombre();
     }
     
 }
