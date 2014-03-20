76a6a412b03e15f3cc1abdf0e2ce3e715fa0e478
Mike <email@email.com>
2014-03-20 13:51:19 +0100

    commit de Miguel (2)

--- pruebagit/src/pruebagit/Pruebagit.java
+++ pruebagit/src/pruebagit/Pruebagit.java
@@ -16,7 +16,8 @@ public class Pruebagit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        PruebaMike obj = new PruebaMike();
+        obj.imprimirNombre();
     }
     
 }
