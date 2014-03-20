e00a01408d27d6553fa8be42d82e8068dd7527e2
Mike <email@email.com>
2014-03-20 13:49:06 +0100

    commit de Miguel

---
+++ pruebagit/src/pruebagit/PruebaMike.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebagit;
+
+/**
+ *
+ * @author usuario
+ */
+public class PruebaMike {
+    public void imprimirNombre() {
+        System.out.println("Michele Rauzino");
+    }
+}
